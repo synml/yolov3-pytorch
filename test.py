b000d03074f017ed4475b8681d6914bf166c306f
Lulin <kistssy+dev@gmail.com>
2020-04-27 01:50:21 +0900

    refactor: reformat code

--- test.py
+++ test.py
@@ -91,6 +91,6 @@ if __name__ == "__main__":
 
     print("Average Precisions:")
     for i, c in enumerate(ap_class):
-        print(f"+ Class '{c}' ({class_names[c]}) - AP: {round(AP[i] * 100, 2)}")
+        print("+ Class '{}' ({}) - AP: {}".format(c, class_names[c], round(AP[i] * 100, 2)))
 
     print(f"mAP: {AP.mean()}")
