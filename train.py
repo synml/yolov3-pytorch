6124388f20e4bfcb82def4f07feb1e1aff4058f2
LulinPollux <kistssy+dev@gmail.com>
2020-04-27 17:23:41 +0900

    train: add default value of pretrained_weights argument
    also I reformat code for better readability.

--- train.py
+++ train.py
@@ -18,18 +18,30 @@ from torch.autograd import Variable
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=100, help="number of epochs")
-    parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
-    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
-    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
-    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
-    parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
-    parser.add_argument("--compute_map", default=False, help="if True computes mAP every tenth batch")
-    parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="number of epochs")
+    parser.add_argument("--batch_size", type=int, default=8,
+                        help="size of each image batch")
+    parser.add_argument("--gradient_accumulations", type=int, default=2,
+                        help="number of gradient accums before step")
+    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg",
+                        help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data",
+                        help="path to data config file")
+    parser.add_argument("--pretrained_weights", type=str, default='weights/darknet53.conv.74',
+                        help="if specified starts from checkpoint model")
+    parser.add_argument("--n_cpu", type=int, default=8,
+                        help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416,
+                        help="size of each image dimension")
+    parser.add_argument("--checkpoint_interval", type=int, default=1,
+                        help="interval between saving model weights")
+    parser.add_argument("--evaluation_interval", type=int, default=1,
+                        help="interval evaluations on validation set")
+    parser.add_argument("--compute_map", default=False,
+                        help="if True computes mAP every tenth batch")
+    parser.add_argument("--multiscale_training", default=True,
+                        help="allow for multi-scale training")
     opt = parser.parse_args()
     print(opt)
 
