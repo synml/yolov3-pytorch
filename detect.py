ae3fb3b4325d86e6897f05ef33948ed18d81c59b
LulinPollux <kistssy+dev@gmail.com>
2020-04-29 19:46:07 +0900

    detect: add argument to selecting save folder

--- detect.py
+++ detect.py
@@ -25,15 +25,14 @@ if __name__ == "__main__":
     parser.add_argument("--class_path", type=str, default="../../data/coco/coco_classes.txt", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
-    parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
-    parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--save_folder", type=str, default='../../detect', help='path to saving result folder')
     args = parser.parse_args()
     print(args)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    os.makedirs("output", exist_ok=True)
 
     # Set up model
     model = Darknet(args.model_def, img_size=args.img_size).to(device)
@@ -76,7 +75,7 @@ if __name__ == "__main__":
         current_time = time.time()
         inference_time = datetime.timedelta(seconds=current_time - prev_time)
         prev_time = current_time
-        print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+        print("\tBatch %d, Inference Time: %s" % (batch_i, inference_time))
 
         # Save image and detections
         imgs.extend(img_paths)
@@ -85,6 +84,8 @@ if __name__ == "__main__":
     # Bounding-box colors
     cmap = plt.get_cmap("tab20b")
     colors = [cmap(i) for i in np.linspace(0, 1, 20)]
+
+    os.makedirs("output", exist_ok=True)
 
     print("\nSaving images:")
     # Iterate through images and save plot of detections
@@ -110,7 +111,7 @@ if __name__ == "__main__":
             bbox_colors = random.sample(colors, n_cls_preds)
             for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
 
-                print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
+                print("\tLabel: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
 
                 box_w = x2 - x1
                 box_h = y2 - y1
@@ -121,19 +122,16 @@ if __name__ == "__main__":
                 # Add the bbox to the plot
                 ax.add_patch(bbox)
                 # Add label
-                plt.text(
-                    x1,
-                    y1,
-                    s=classes[int(cls_pred)],
-                    color="white",
-                    verticalalignment="top",
-                    bbox={"color": color, "pad": 0},
-                )
+                plt.text(x1, y1,
+                         s=classes[int(cls_pred)],
+                         color="white",
+                         verticalalignment="top",
+                         bbox={"color": color, "pad": 0})
 
         # Save generated image with detections
         plt.axis("off")
         plt.gca().xaxis.set_major_locator(NullLocator())
         plt.gca().yaxis.set_major_locator(NullLocator())
         filename = path.split("/")[-1].split(".")[0]
-        plt.savefig(f"output/{filename}.png", bbox_inches="tight", pad_inches=0.0)
+        plt.savefig("{}/{}.png".format(args.save_folder, filename), bbox_inches="tight", pad_inches=0.0)
         plt.close()
