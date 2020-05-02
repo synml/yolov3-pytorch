951df47e20925e86c70abc528ddf9ba78d8646dd
LulinPollux <kistssy+dev@gmail.com>
2020-05-02 16:36:38 +0900

    refactor: reformat files and delete useless code

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -22,8 +22,6 @@ def parse_model_config(path):
 def parse_data_config(path):
     """Parses the data configuration file"""
     options = dict()
-    options['gpus'] = '0,1,2,3'
-    options['num_workers'] = '10'
     with open(path, 'r') as fp:
         lines = fp.readlines()
     for line in lines:
