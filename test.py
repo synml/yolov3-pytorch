9a087e6fa06d1ab3597a1d8b3079a8c176828c10
Lulin <kistssy+dev@gmail.com>
2020-05-03 17:56:41 +0900

    test: fix uncontrollable batch size

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=512, help="size of each image batch")
+    parser.add_argument("--batch_size", type=int, default=2, help="size of each image batch")
     parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
     parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         conf_thres=args.conf_thres,
         nms_thres=args.nms_thres,
         img_size=args.img_size,
-        batch_size=8,
+        batch_size=args.batch_size,
     )
 
     # Print AP and mAP.
