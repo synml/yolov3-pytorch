76d4b70ccde6ec033285f911c17cb866600f7ceb
LulinPollux <kistssy+dev@gmail.com>
2020-05-10 03:34:26 +0900

    Update setup_coco_dataset.sh
    add links

--- data/setup_coco_dataset.sh
+++ data/setup_coco_dataset.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# You have to download original files.
-# Download link will provide later... (but you can get them through Googling and little modifications.)
+# You have to download archive file related to yolov3's coco dataset.
+# Download link: https://drive.google.com/file/d/1wJn0GOGcWpHwe60tN3lP63cb28lEqemJ/view?usp=sharing
+# And move the archive file to coco dataset directory.
+# coco dataset directory is ../../data/coco (start at this project directory)
 
 # cd to dataset directory.
 cd ../../data/coco
+
+# Download coco dataset archive files.
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
 
 # Make images directory and cd.
 mkdir images
@@ -18,6 +24,10 @@ rm ../val2014.zip
 
 # cd to ../../data/coco
 cd ..
+
+# Unzip archive file related to yolov3's coco dataset.
+unzip yolov3.zip
+rm yolov3.zip
 
 # Move list files and unzip annotations file.
 mv ./yolov3/*.txt ./
