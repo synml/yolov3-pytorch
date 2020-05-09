f99c7e01e19f1b247dcefcfafc944c95f18df2bf
LulinPollux <kistssy+dev@gmail.com>
2020-05-10 05:24:10 +0900

    utils: change the initialization function from normal to kaiming
    Kaiming He 초기화 개좋음!!!!

--- utils/utils.py
+++ utils/utils.py
@@ -19,7 +19,7 @@ def load_classes(path):
 def weights_init_normal(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+        torch.nn.init.kaiming_normal_(m.weight.data, 0.1)
     elif classname.find("BatchNorm2d") != -1:
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
