94a24b19ced861f1f963bbffde4d4e260143093d
LulinPollux <kistssy+dev@gmail.com>
2020-05-12 16:02:54 +0900

    Revert "model: refactor parameters"
    This reverts commit 3a27d46bf4468dcc9a6aca0f8841bf1ab0123c86.

--- models.py
+++ models.py
@@ -60,8 +60,9 @@ def create_modules(module_defs, img_size: int):
             anchors = [(anchors[i], anchors[i + 1]) for i in range(0, len(anchors), 2)]
             anchors = [anchors[i] for i in anchor_idxs]
             num_classes = int(module_def["classes"])
+            img_size = img_size
             # Define detection layer
-            yolo_layer = YOLOLayer(anchors, num_classes)
+            yolo_layer = YOLOLayer(anchors, num_classes, img_size)
             modules.add_module(f"yolo_{module_i}", yolo_layer)
         # Register module list and number of output filters
         module_list.append(modules)
@@ -93,7 +94,7 @@ class EmptyLayer(nn.Module):
 class YOLOLayer(nn.Module):
     """Detection layer"""
 
-    def __init__(self, anchors, num_classes: int):
+    def __init__(self, anchors, num_classes: int, img_dim: int):
         super(YOLOLayer, self).__init__()
         self.anchors = anchors
         self.num_anchors = len(anchors)
@@ -104,13 +105,14 @@ class YOLOLayer(nn.Module):
         self.obj_scale = 1
         self.noobj_scale = 100
         self.metrics = {}
+        self.img_dim = img_dim
         self.grid_size = 0  # grid size
 
-    def compute_grid_offsets(self, img_dim, grid_size, cuda=True):
+    def compute_grid_offsets(self, grid_size, cuda=True):
         self.grid_size = grid_size
         g = self.grid_size
         FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-        self.stride = img_dim / self.grid_size
+        self.stride = self.img_dim / self.grid_size
         # Calculate offsets for each grid
         self.grid_x = torch.arange(g).repeat(g, 1).view([1, 1, g, g]).type(FloatTensor)
         self.grid_y = torch.arange(g).repeat(g, 1).t().view([1, 1, g, g]).type(FloatTensor)
@@ -118,11 +120,12 @@ class YOLOLayer(nn.Module):
         self.anchor_w = self.scaled_anchors[:, 0:1].view((1, self.num_anchors, 1, 1))
         self.anchor_h = self.scaled_anchors[:, 1:2].view((1, self.num_anchors, 1, 1))
 
-    def forward(self, x, targets, img_dim):
+    def forward(self, x, targets=None, img_dim=None):
 
         # Tensors for cuda support
         FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
 
+        self.img_dim = img_dim
         num_samples = x.size(0)
         grid_size = x.size(2)
 
@@ -139,7 +142,7 @@ class YOLOLayer(nn.Module):
         pred_conf = torch.sigmoid(prediction[..., 4])  # Conf
         pred_cls = torch.sigmoid(prediction[..., 5:])  # Cls pred.
 
-        self.compute_grid_offsets(img_dim, grid_size, cuda=cx.is_cuda)
+        self.compute_grid_offsets(grid_size, cuda=cx.is_cuda)
 
         # Add offset and scale with anchors
         pred_boxes = FloatTensor(prediction[..., :4].shape)
