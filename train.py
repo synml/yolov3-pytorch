449a6dc42dfa56525a93358c0802c1f3614909a4
LulinPollux <kistssy+dev@gmail.com>
2020-05-15 15:10:00 +0900

    train: refactor code
    reformat code

--- train.py
+++ train.py
@@ -15,24 +15,16 @@ from utils.parse_config import *
 from test import evaluate
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epochs", type=int, default=200,
-                    help="number of epochs")
-parser.add_argument("--batch_size", type=int, default=16,
-                    help="size of each image batch")
-parser.add_argument("--gradient_accumulations", type=int, default=2,
-                    help="number of gradient accums before step")
-parser.add_argument("--model_def", type=str, default="config/yolov3.cfg",
-                    help="path to model definition file")
-parser.add_argument("--data_config", type=str, default="config/coco.data",
-                    help="path to data config file")
+parser.add_argument("--epochs", type=int, default=200, help="number of epochs")
+parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
+parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
+parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
 parser.add_argument("--pretrained_weight", type=str, default='weights/darknet53.conv.74',
                     help="if specified starts from checkpoint model")
-parser.add_argument("--n_cpu", type=int, default=8,
-                    help="number of cpu threads to use during batch generation")
-parser.add_argument("--img_size", type=int, default=416,
-                    help="size of each image dimension")
-parser.add_argument("--multiscale_training", default=True,
-                    help="allow for multi-scale training")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
 args = parser.parse_args()
 print(args)
 
@@ -118,8 +110,8 @@ for epoch in tqdm.tqdm(range(args.epochs), desc='Epoch'):
         loss, outputs = model(imgs, targets)
         loss.backward()
 
+        # Accumulates gradient before each step
         if step % args.gradient_accumulations:
-            # Accumulates gradient before each step
             optimizer.step()
             optimizer.zero_grad()
 
