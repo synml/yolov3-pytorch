8d4bec7a0675e88d3d75fefa56e6cfb12ae147b0
LulinPollux <kistssy+dev@gmail.com>
2020-05-26 10:05:31 +0900

    train: refactor entire code

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads 
 args = parser.parse_args()
 print(args)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 now = time.strftime('%y%m%d_%H%M%S', time.localtime(time.time()))
 
 # Tensorboard writer 객체 생성
@@ -36,9 +36,9 @@ writer = torch.utils.tensorboard.SummaryWriter(log_dir)
 
 # Get data configuration
 data_config = utils.parse_config.parse_data_config(args.data_config)
-train_path = data_config["train"]
-valid_path = data_config["valid"]
-class_names = load_classes(data_config["names"])
+train_path = data_config['train']
+valid_path = data_config['valid']
+class_names = load_classes(data_config['names'])
 
 # Initiate model
 model = YOLOv3(args.img_size, int(data_config['classes'])).to(device)
@@ -46,7 +46,7 @@ model.apply(init_weights_normal)
 
 # If specified we start from checkpoint
 if args.pretrained_weights:
-    if args.pretrained_weights.endswith(".pth"):
+    if args.pretrained_weights.endswith('.pth'):
         model.load_state_dict(torch.load(args.pretrained_weights))
     else:
         model.load_darknet_weights(args.pretrained_weights)
@@ -73,8 +73,8 @@ loss_log = tqdm.tqdm(total=0, position=2, bar_format='{desc}', leave=False)
 for epoch in tqdm.tqdm(range(args.epochs), desc='Epoch'):
     model.train()
 
-    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc='Batch', leave=False)):
-        step = len(dataloader) * epoch + batch_i
+    for batch_idx, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc='Batch', leave=False)):
+        step = len(dataloader) * epoch + batch_idx
 
         imgs = imgs.to(device)
         targets = targets.to(device)
@@ -93,10 +93,8 @@ for epoch in tqdm.tqdm(range(args.epochs), desc='Epoch'):
         # Tensorboard logging
         tensorboard_log = []
         for i, yolo_layer in enumerate(model.yolo_layers):
-            tensorboard_log.append((f"{'layer_loss'}_{i + 1}", yolo_layer.metrics['layer_loss']))
-        tensorboard_log.append(("total_loss", loss.item()))
-        for tag, value in tensorboard_log:
-            writer.add_scalar(tag, value, step)
+            writer.add_scalar('layer_loss_{}'.format(i + 1), yolo_layer.metrics['layer_loss'], step)
+        writer.add_scalar('total_loss', loss.item(), step)
 
     scheduler.step()
 
@@ -109,17 +107,14 @@ for epoch in tqdm.tqdm(range(args.epochs), desc='Epoch'):
                                                    img_size=args.img_size,
                                                    batch_size=args.batch_size,
                                                    num_workers=args.n_cpu)
-    evaluation_metrics = [
-        ("val_precision", precision.mean()),
-        ("val_recall", recall.mean()),
-        ("val_mAP", AP.mean()),
-        ("val_f1", f1.mean()),
-    ]
-    for tag, value in evaluation_metrics:
-        writer.add_scalar(tag, value, epoch)
+
+    writer.add_scalar('val_precision', precision.mean(), epoch)
+    writer.add_scalar('val_recall', recall.mean(), epoch)
+    writer.add_scalar('val_mAP', AP.mean(), epoch)
+    writer.add_scalar('val_f1', f1.mean(), epoch)
 
     # Save checkpoint file
     save_dir = os.path.join('checkpoints', now)
     os.makedirs(save_dir, exist_ok=True)
     dataset_name = os.path.split(args.data_config)[-1].split('.')[0]
-    torch.save(model.state_dict(), os.path.join(save_dir, "yolov3_{}_{}.pth".format(dataset_name, epoch)))
+    torch.save(model.state_dict(), os.path.join(save_dir, 'yolov3_{}_{}.pth'.format(dataset_name, epoch)))
