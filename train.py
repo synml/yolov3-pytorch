21a226ad221291deda9a424ce22d085b619504bf
Lulin <kistssy+dev@gmail.com>
2020-06-08 23:16:41 +0900

    train: Fix issue about too many values to unpack

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ for epoch in tqdm.tqdm(range(args.epochs), desc='Epoch'):
     model.train()
 
     # 1 epoch의 각 배치에서 처리하는 코드
-    for batch_idx, (imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc='Batch', leave=False)):
+    for batch_idx, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc='Batch', leave=False)):
         step = len(dataloader) * epoch + batch_idx
 
         # 이미지와 정답 정보를 GPU로 복사
